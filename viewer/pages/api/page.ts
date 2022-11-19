1f3ad4cf04d2086efe2d92ecd9be585fe4d1c5d1
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2022-11-19 23:30:16 +0200

    Add very slow prototype of resolving links

--- viewer/pages/api/page.ts
+++ viewer/pages/api/page.ts
@@ -2,6 +2,18 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import fs from "fs";
 import { sortedByDate } from "@/utils/sortedByDate";
+import { exec } from "child_process";
+
+async function getUrl(url: string) {
+  return new Promise((resolve, reject) =>
+    exec(
+      `curl -Ls -o /dev/null -w %{url_effective} ${url}`,
+      (error, stdout, stderr) => {
+        resolve(stdout);
+      }
+    )
+  );
+}
 
 type Data = any;
 
@@ -22,37 +34,60 @@ export default async function handler(
     await fs.promises.readFile(`../likes/${directory[Number(number)]}`, "utf-8")
   );
 
-  const tweets = file.data.map((tweet) => {
-    return {
-      id: tweet.id,
-      text: tweet.text,
-      ...(tweet.attachments && tweet.attachments.media_keys
-        ? {
-            attachments: tweet.attachments.media_keys.map((attachment) => {
-              return file.includes.media.find(
-                (media) => media.media_key === attachment
-              );
-            }),
-          }
-        : {}),
-      ...(tweet.referenced_tweets
-        ? {
-            referenced_tweets: tweet.referenced_tweets.map(
-              (referenced_tweet) => {
-                const referenced = file.includes.tweets.find(
-                  (tweet) => tweet.id === referenced_tweet.id
-                );
-                // TODO: add author.
+  const tweets = await Promise.all(
+    file.data.map(async (tweet) => {
+      const urlsInText = [...tweet.text.matchAll(/(https?:\/\/[^\s]+)/g)].map(
+        (m) => m[0]
+      );
 
-                return { ...referenced, type: referenced_tweet.type };
-              }
-            ),
-          }
-        : {}),
-      user: file.includes.users.find((user) => user.id === tweet.author_id),
-      created_at: tweet.created_at,
-    };
-  });
+      const resolvedUrls = await Promise.all(
+        urlsInText.map((url) => getUrl(url))
+      );
+
+      const mapping = {};
+      for (let i = 0; i < urlsInText.length; i++) {
+        mapping[urlsInText[i]] = resolvedUrls[i];
+      }
+
+      const replacedText = tweet.text.replace(/(https?:\/\/[^\s]+)/g, (url) => {
+        return mapping[url];
+      });
+
+      return {
+        id: tweet.id,
+        text: replacedText,
+        ...(tweet.attachments && tweet.attachments.media_keys
+          ? {
+              attachments: tweet.attachments.media_keys.map((attachment) => {
+                return file.includes.media.find(
+                  (media) => media.media_key === attachment
+                );
+              }),
+            }
+          : {}),
+        ...(tweet.referenced_tweets
+          ? {
+              referenced_tweets: tweet.referenced_tweets.map(
+                (referenced_tweet) => {
+                  const referenced = file.includes.tweets.find(
+                    (tweet) => tweet.id === referenced_tweet.id
+                  );
+
+                  const author = file.includes.users.find(
+                    (user) => user.id === referenced.author_id
+                  );
+                  // TODO: add author.
+
+                  return { ...referenced, author, type: referenced_tweet.type };
+                }
+              ),
+            }
+          : {}),
+        user: file.includes.users.find((user) => user.id === tweet.author_id),
+        created_at: tweet.created_at,
+      };
+    })
+  );
 
   res.status(200).json(tweets);
 }
--- viewer/pages/index.tsx
+++ viewer/pages/index.tsx
@@ -1,13 +1,104 @@
 import { sortedByDate } from "@/utils/sortedByDate";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
+import { format } from "date-fns";
+
+const Avatar = ({ user }) => {
+  return (
+    <img
+      className="rounded-full w-12 h-12"
+      src={user.profile_image_url}
+      alt={`Avatar of ${user.name}`}
+    />
+  );
+};
+
+const TopBar = ({ user, created_at }) => {
+  return (
+    <div className="flex gap-1 break-keep">
+      <div className="text-white font-bold whitespace-nowrap text-ellipsis overflow-hidden">
+        {user.name}
+      </div>
+      <div>
+        <Link
+          target="_blank"
+          rel="noreferrer"
+          href={`https://twitter.com/${user.username}`}
+        >
+          @{user.username}
+        </Link>
+      </div>
+      ·<div>{format(new Date(created_at), "do MMM yyyy")}</div>
+    </div>
+  );
+};
+
+const StyledLink = ({
+  href,
+  children,
+}: {
+  href: string;
+  children: ReactNode;
+}) => {
+  return (
+    <Link
+      target="_blank"
+      rel="noreferrer"
+      href={href}
+      className="rounded text-slate-100 decoration-slate-300 decoration-2 underline-offset-4 outline-none hover:underline focus-visible:ring-2 focus-visible:ring-sky-500 dark:decoration-slate-600"
+    >
+      {children}
+    </Link>
+  );
+};
+
+const FormatTweet = ({ tweet }: { tweet: string }) => {
+  const split = tweet
+    .replace(/(https?:\/\/[^\s]+)/g, "§$1§")
+    .replace(/(^|\s)@(\w+)/g, "$1§@$2§")
+    .replace(/(^|\s)#(\w+)/g, "$1§#$2§");
+
+  return (
+    <div>
+      {split.split("§").map((part, i) => {
+        if (part.startsWith("http")) {
+          return (
+            <StyledLink key={i} href={part}>
+              {part}
+            </StyledLink>
+          );
+        }
+
+        if (part.startsWith("@")) {
+          return (
+            <StyledLink key={i} href={`https://twitter.com/${part.slice(1)}`}>
+              {part}
+            </StyledLink>
+          );
+        }
+
+        if (part.startsWith("#")) {
+          return (
+            <StyledLink
+              key={i}
+              href={`https://twitter.com/hashtag/${part.slice(1)}`}
+            >
+              {part}
+            </StyledLink>
+          );
+        }
+
+        return <span key={i} dangerouslySetInnerHTML={{ __html: part }} />;
+      })}
+    </div>
+  );
+};
 
 export const getStaticProps = async () => {
   const directory = await sortedByDate();
 
   return {
     props: {
-      hello: "world",
       directory,
     },
   };
@@ -34,28 +125,21 @@ export default function Index(
       });
   }, [page]);
 
-  const formatTweet = (tweet: string) => {
-    return tweet
-      .replace(/(https?:\/\/[^\s]+)/g, '<a href="$1" target="_blank">$1</a>')
-      .replace(
-        /(^|\s)@(\w+)/g,
-        '$1<a href="https://twitter.com/$2" target="_blank">@$2</a>'
-      )
-      .replace(
-        /(^|\s)#(\w+)/g,
-        '$1<a href="https://twitter.com/hashtag/$2" target="_blank">#$2</a>'
-      );
-  };
-
   const mapReferencedTypeToLabel = {
     replied_to: "Replied to",
     quoted: "Quoted",
   };
 
+  // TODO:
+  // - Images need some border, maybe.
+  // - Long username can make tweet overflow.
+  // - Style replied and quoted tweets.
+  // - Make sure that referenced tweets have full width.
+
   return (
     <div className="flex justify-center bg-black">
       <div className="flex flex-col w-600px">
-        <div className="flex items-center gap-4 p-4 text-white border-l border-r border-slate-900">
+        <div className="flex items-center gap-4 p-4 text-white border-l border-r border-slate-800">
           <div>
             Current page: <strong>{page}</strong> Total pages:{" "}
           </div>
@@ -75,90 +159,75 @@ export default function Index(
             Next
           </button>
         </div>
-        <div className="flex flex-col divide-y border-l border-t border-r border-slate-900 divide-slate-900">
+        <div className="flex flex-col divide-y border-l border-t border-r border-slate-800 divide-slate-800">
           {data?.map((tweet) => {
             return (
               <div key={tweet.id} className="text-slate-400 flex flex-col p-4">
                 <div className="flex gap-3">
-                  <img
-                    className="rounded-full w-12 h-12"
-                    src={tweet.user.profile_image_url}
-                    alt={`Avatar of ${tweet.user.name}`}
-                  />
+                  <Avatar user={tweet.user} />
                   <div className="flex flex-col">
-                    <div className="flex gap-1 whitespace-nowrap break-keep">
-                      <div className="text-white font-bold ">
-                        {tweet.user.name}
-                      </div>
+                    <TopBar user={tweet.user} created_at={tweet.created_at} />
+                    <FormatTweet tweet={tweet.text} />
+                    <div className="mt-4 flex flex-col gap-4">
+                      {tweet.attachments && (
+                        <div className="flex flex-col gap-4">
+                          {tweet.attachments.map((attachment) => {
+                            if (attachment.type === "photo") {
+                              return (
+                                <img
+                                  className="rounded overflow-hidden"
+                                  src={attachment.url}
+                                  alt="Tweet attachment"
+                                />
+                              );
+                            }
+                          })}
+                        </div>
+                      )}
+                      {tweet.referenced_tweets && (
+                        <div className="flex flex-col gap-4">
+                          {tweet.referenced_tweets.map((referenced_tweet) => {
+                            return (
+                              <div
+                                key={referenced_tweet.id}
+                                className="flex flex-col gap-2"
+                              >
+                                <div>
+                                  {
+                                    mapReferencedTypeToLabel[
+                                      referenced_tweet.type
+                                    ]
+                                  }
+                                  :
+                                </div>
+                                <div className="p-4 rounded bg-slate-900 flex gap-4">
+                                  <Avatar user={referenced_tweet.author} />
+                                  <div className="flex flex-col gap-2">
+                                    {referenced_tweet.author && (
+                                      <TopBar
+                                        user={referenced_tweet.author}
+                                        created_at={referenced_tweet.created_at}
+                                      />
+                                    )}
+                                    <FormatTweet
+                                      tweet={referenced_tweet.text}
+                                    />
+                                  </div>
+                                </div>
+                              </div>
+                            );
+                          })}
+                        </div>
+                      )}
                       <div>
-                        <Link
-                          target="_blank"
-                          rel="noreferrer"
-                          href={`https://twitter.com/${tweet.user.username}`}
-                        >
-                          @{tweet.user.username}
-                        </Link>
-                      </div>
-                      <div>{tweet.created_at}</div>
-                      <div>
-                        <Link
-                          target="_blank"
-                          rel="noreferrer"
+                        <StyledLink
                           href={`https://twitter.com/${tweet.user.username}/status/${tweet.id}`}
                         >
-                          Tweet
-                        </Link>
+                          Go to tweet{" "}
+                          <span className="text-slate-500">-&gt;</span>
+                        </StyledLink>
                       </div>
                     </div>
-                    <div
-                      dangerouslySetInnerHTML={{
-                        // TODO: find usernames.
-                        __html: formatTweet(tweet.text),
-                      }}
-                    />
-                    {tweet.attachments && (
-                      <div className="mt-4 flex flex-col gap-4">
-                        {tweet.attachments.map((attachment) => {
-                          if (attachment.type === "photo") {
-                            return (
-                              <img
-                                className="rounded overflow-hidden"
-                                src={attachment.url}
-                                alt="Tweet attachment"
-                              />
-                            );
-                          }
-                        })}
-                      </div>
-                    )}
-                    {tweet.referenced_tweets && (
-                      <div className="mt-4 flex flex-col gap-4">
-                        {tweet.referenced_tweets.map((referenced_tweet) => {
-                          return (
-                            <div
-                              key={referenced_tweet.id}
-                              className="flex flex-col gap-2"
-                            >
-                              <div>
-                                Type:{" "}
-                                {
-                                  mapReferencedTypeToLabel[
-                                    referenced_tweet.type
-                                  ]
-                                }
-                              </div>
-                              <div
-                                className="p-4 rounded bg-slate-900"
-                                dangerouslySetInnerHTML={{
-                                  __html: formatTweet(referenced_tweet.text),
-                                }}
-                              />
-                            </div>
-                          );
-                        })}
-                      </div>
-                    )}
-                    {/* <div>{JSON.stringify(tweet.attachments)}</div> */}
                   </div>
                 </div>
               </div>
