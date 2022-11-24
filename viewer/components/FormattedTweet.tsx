d79ecc0495789531db08d4bae04c5642815ebd22
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2022-11-24 13:34:29 +0200

    Filter out links of attachments in displaying tweets

--- viewer/components/FormattedTweet.tsx
+++ viewer/components/FormattedTweet.tsx
@@ -2,9 +2,18 @@ import { StyledLink } from "./StyledLink";
 
 export function FormattedTweet({ tweet }: { tweet: string }) {
   const split = tweet
+    // Filter out images and videos which are attached.
+    .replace(
+      /https:\/\/twitter\.com\/[A-z]+\/status\/\d+\/(photo|video)\/\d+$/g,
+      ""
+    )
+    // Mark links.
     .replace(/(https?:\/\/[^\s]+)/g, "§$1§")
+    // Mark user handles.
     .replace(/(^|\s|\.)@(\w+)/g, "$1§@$2§")
+    // Mark hashtags.
     .replace(/(^|\s)#(\w+)/g, "$1§#$2§")
+    // Split on new lines.
     .replace(/\n/g, "<br />");
 
   return (
